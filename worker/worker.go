46df4df0674bf624fbda35f4f90c026be7f7e5e8
Ben Schwartz <schwartzb.2019@gmail.com>
2023-02-04 21:35:57 -0500

    update placeholder

--- worker/worker.go
+++ worker/worker.go
@@ -2,12 +2,13 @@ package worker
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/19chonm/461_1_23/fileio"
 )
 
 func runTask(url string, ratingch chan<- fileio.Rating) {
 	fmt.Println("My job is", url)
-	r := fileio.Rating{NetScore: 5.0, Url: "url_for_module_A"}
+	r := fileio.Rating{NetScore: rand.Float64(), Url: url} // Placeholder until real data implemented
 	ratingch <- r
 }
