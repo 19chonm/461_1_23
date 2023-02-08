7625523b5431a4fa7fc9574d514fef0e6a19eb1b
Ben Schwartz <schwartzb.2019@gmail.com>
2023-02-07 22:01:43 -0500

    revamp formula calc

---
+++ metrics/formulaCalculator.go
@@ -0,0 +1,77 @@
+package metrics
+
+import (
+	"math"
+)
+
+type Factor struct {
+	Weight       float64
+	Value        float64
+	AllOrNothing bool
+}
+
+func ComputeNetScore(fs []Factor) float64 {
+	var sum float64
+
+	for _, f := range fs {
+		if f.AllOrNothing {
+			if f.Value == 0 {
+				sum = 0
+				break
+			} else {
+				continue
+			}
+		}
+
+		sum += f.Value * f.Weight
+	}
+
+	return sum
+}
+
+func ComputeRampTime(found int, total int) float64 {
+	// Compute Ramp-up time based on number of phrases found in README
+	if total == 0 {
+		return 0
+	}
+	return float64(found) / float64(total)
+}
+
+func ComputeCorrectness(clones int, views int, commits int) float64 {
+	// Compute the correctness score based on number of repository
+	// clones, page views, and number of total commits
+	var cs, vs, ms float64
+	cs = 0.117 * (1 - math.Exp(-0.001*float64(clones)))   // 2k clones for 80%
+	vs = 0.550 * (1 - math.Exp(-0.00002*float64(clones))) // 100k views for 86%
+	ms = 0.333 * (1 - math.Exp(-0.0005*float64(clones)))  // 6000 commits for 90% of this
+
+	return cs + vs + ms
+}
+
+func ComputeResponsiveness(days float64) float64 {
+	// Compute the responsiveness score based on average
+	// number of days to fix bug issues
+	if days < 0 {
+		return 1
+	}
+
+	return math.Exp(-0.05 * float64(days))
+}
+
+func ComputeBusFactor(total int, p1 int, p2 int, p3 int) float64 {
+	// Compute the Bus factor by measuring the percentage of commits
+	// in the past year committed by the top three performers
+	if total <= 0 {
+		return 0
+	}
+
+	return 1 - (float64(p1+p2+p3) / float64(total))
+}
+
+func ComputeLicenseScore(license string) int {
+	if license == "MIT License" {
+		return 1
+	} else {
+		return 0
+	}
+}
--- worker/worker.go
+++ worker/worker.go
@@ -2,7 +2,6 @@ package worker
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/19chonm/461_1_23/api"
 	"github.com/19chonm/461_1_23/fileio"
@@ -11,7 +10,6 @@ import (
 
 func runTask(url string, ratingch chan<- fileio.Rating) {
 	fmt.Println("My job is", url)
-	rampupscore := metrics.ScanRepo(url)
 
 	l := api.GetRepoLicense(url)
 	fmt.Println("license: ", *l.License.Name)
@@ -20,6 +18,33 @@ func runTask(url string, ratingch chan<- fileio.Rating) {
 	c := api.GetRepoContributors(url)
 	fmt.Println("contributors: ", c)
 
-	rating := fileio.Rating{NetScore: rand.Float64(), Rampup: rampupscore, Url: url} // Placeholder until real data implemented
-	ratingch <- rating
+	// rating := fileio.Rating{NetScore: rand.Float64(), Rampup: rampupscore, Url: url} // Placeholder until real data implemented
+	// ratingch <- rating
+
+	rampup_score := metrics.ScanRepo(url)
+	correctness_score := metrics.ComputeCorrectness(0, 0, 0)
+	responsiveness_score := metrics.ComputeResponsiveness(0)
+	busfactor_score := metrics.ComputeBusFactor(0, 0, 0, 0)
+	license_score := metrics.ComputeLicenseScore("MIT License")
+
+	rampup_factor := metrics.Factor{Weight: 0.15, Value: rampup_score, AllOrNothing: false}
+	correctness_factor := metrics.Factor{Weight: 0.15, Value: correctness_score, AllOrNothing: false}
+	responsiveness_factor := metrics.Factor{Weight: 0.4, Value: responsiveness_score, AllOrNothing: false}
+	busfactor_factor := metrics.Factor{Weight: 0.3, Value: busfactor_score, AllOrNothing: false}
+	license_factor := metrics.Factor{Weight: 1.0, Value: float64(license_score), AllOrNothing: true}
+
+	// Assuming only valid urls get passed through
+	// url_factor := metrics.Factor{Weight: 1.0, Value: float64(url_score), AllOrNothing: true}
+
+	factors := []metrics.Factor{rampup_factor, correctness_factor, responsiveness_factor, busfactor_factor, license_factor}
+
+	r := fileio.Rating{NetScore: metrics.ComputeNetScore(factors),
+		Rampup:         rampup_score,
+		Url:            url,
+		License:        float64(license_score),
+		Busfactor:      busfactor_score,
+		Responsiveness: responsiveness_score,
+		Correctness:    correctness_score,
+	}
+	ratingch <- r
 }
