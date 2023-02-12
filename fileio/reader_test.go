152ff883a86c45d82b862c9c16de397ae89455f1
Ashassins <ashenassassinmc@gmail.com>
2023-02-12 16:23:09 -0500

    ADD: IsValidURL tests

--- fileio/reader_test.go
+++ fileio/reader_test.go
@@ -4,10 +4,25 @@ import (
 	"testing"
 )
 
-// func Test_(t *testing.T) {
+func Test_(t *testing.T) {
 
-// }
+}
 
+func Test_IsValidURL_Success(t *testing.T) {
+	url_str := "https://github.com/facebook/react"
+	valid := IsValidURL(url_str)
+	if valid == false {
+		t.Errorf("want %t, got %t.", true, valid)
+	}
+}
+
+func Test_IsValidURl_Fail(t *testing.T) {
+	bad_url_str := "https://google.com/fakebok/real"
+	valid := IsValidURL(bad_url_str)
+	if valid == true {
+		t.Errorf("want %t, got %t.", false, valid)
+	}
+}
 
 func Test_MakeUrlChannel_Success(t *testing.T) {
 	result := MakeUrlChannel() 
